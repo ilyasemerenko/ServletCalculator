8740a4170e6ee49d419638a56b8ce064fa4e9b00
Ilya Semerenko <Ilya.Semerenko@areon.ua>
2016-03-23 15:32:39 +0200

    Some fixes after review

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,24 +1,12 @@
 import javax.servlet.http.HttpServletRequest;
-import java.math.BigDecimal;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.ArrayList;
+import java.util.List;
 
-/**
- * Created by SemerenkoIlya on 22.03.2016.
- */
 public class Calculator {
-    private static HashMap<String,Integer> operMap;
     private String operation;
-    static{
-        operMap = new HashMap();
-        operMap.put("add",1);
-        operMap.put("sub",-1);
-        operMap.put("mul",2);
-        operMap.put("div",-2);
-    }
 
-    private BigDecimal[] parseQuery(HttpServletRequest request){
-        BigDecimal[] operands = new BigDecimal[2];
+    private List<Double> parseQuery(HttpServletRequest request){
+        List<Double> operands = new ArrayList<>();
         String query = request.getQueryString();
         int splitIndex = query.indexOf("&");
         operation = query.substring(query.indexOf("=")+1,splitIndex);
@@ -26,27 +14,27 @@ public class Calculator {
         String first = operandsStr.substring(operandsStr.indexOf("=")+1,operandsStr.indexOf("&"));
         String second = operandsStr.substring(operandsStr.lastIndexOf("=")+1);
         try {
-            operands[0] = BigDecimal.valueOf(Double.valueOf(first));
-            operands[1] = BigDecimal.valueOf(Double.valueOf(second));
+            operands.add(Double.valueOf(first));
+            operands.add(Double.valueOf(second));
         } catch(NumberFormatException e){
             e.printStackTrace();
         }
         return operands;
     }
 
-    public BigDecimal getResult(HttpServletRequest request){
-        BigDecimal [] operands = parseQuery(request);
-        switch (operMap.get(operation)){
-            case 1:
-                return operands[0].add(operands[1]);
-            case -1:
-                return operands[0].subtract(operands[1]);
-            case 2:
-                return operands[0].multiply(operands[1]);
-            case -2:
-                return operands[0].divide(operands[1]);
+    public Double getResult(HttpServletRequest request) throws Exception {
+        List<Double> operands = parseQuery(request);
+        switch (operation){
+            case "add":
+                return operands.get(0) + operands.get(1);
+            case "sub":
+                return operands.get(0) - operands.get(1);
+            case "mul":
+                return operands.get(0) * operands.get(1);
+            case "div":
+                return operands.get(0) / operands.get(1);
             default:
-                return null;
+                throw new Exception("wrong values");
         }
     }
 }
--- src/Servlet.java
+++ src/Servlet.java
@@ -5,16 +5,20 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-/**
- * Created by SemerenkoIlya on 21.03.2016.
- */
 @WebServlet("/calculator")
 public class Servlet extends HttpServlet{
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
         Calculator calc = new Calculator();
         PrintWriter writer = res.getWriter();
-        writer.write("Hello from server! Your result is: " + calc.getResult(req));
+        String result = null;
+        try {
+            result = String.valueOf(calc.getResult(req));
+        } catch (Exception e) {
+            e.printStackTrace();
+            result = "You've entered wrong values or operation";
+        }
+        writer.write("Hello from server! Your result is: " + result);
     }
 
 }
